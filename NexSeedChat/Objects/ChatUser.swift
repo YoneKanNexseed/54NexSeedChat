a344411be38602387159f0dd440ef59839fe8948
obscure723 <obscure723@yone-lab.com>
2019-08-15 10:37:37 +0800

    no message

---
+++ NexSeedChat/Objects/ChatUser.swift
@@ -0,0 +1,9 @@
+//
+//  ChatUser.swift
+//  NexSeedChat
+//
+//  Created by yonekan on 2019/08/15.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import Foundation
---
+++ NexSeedChat/Objects/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  NexSeedChat
+//
+//  Created by yonekan on 2019/08/15.
+//  Copyright © 2019 yonekan. All rights reserved.
+//
+
+import Foundation
